27d8e3f0b4b7e425a6e80cecb9965bb021730d5a
Mathis Barré <mathis.barre@live.fr>
2020-12-22 14:22:33 +0100

    :tada: Repository initialization

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      "backgroundColor" : {
+        oniPink: "#f3e9f1"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
