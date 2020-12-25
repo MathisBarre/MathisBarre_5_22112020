ad3c92d1b0bb60a4b3ba2cac9537e94c0c48a773
Mathis Barré <mathis.barre@live.fr>
2020-12-25 22:26:28 +0100

    fix link url

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function displayProduct(product) {
   // Get template
-  const template = document.getElementById("product")
+  const template = document.getElementById('product')
 
   // Clone template
   const clone = document.importNode(template.content, true)
@@ -8,8 +8,9 @@ function displayProduct(product) {
   // Hydrate template
   clone.getElementById('productImage').src = product.imageUrl
   clone.getElementById('productName').textContent = product.name
-  clone.getElementById('productPrice').textContent = `${product.price / 100} €`
+  clone.getElementById('productPrice').textContent = `${product.price / 100}.00 €`
   clone.getElementById('productDescription').textContent = product.description
+  clone.getElementById('productLink').href = `/products.html?id=${product._id}`
 
   // Display template
   document.getElementById('productsList').appendChild(clone)
@@ -19,7 +20,7 @@ fetch('http://localhost:3000/api/teddies')
   .then(res => res.json())
   .then(products => {
     // Clear loading box
-    document.getElementById("productsList").innerHTML = ""
+    document.getElementById('productsList').innerHTML = ''
 
     // Display each product 
     products.forEach(product => {displayProduct(product)});
