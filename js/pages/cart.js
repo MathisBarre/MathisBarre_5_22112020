2a8821935378f6e026206cd559f3fe15d962fd60
Mathis Barré <mathis.barre@live.fr>
2021-01-29 14:07:19 +0100

    fix: shopping crt page

--- js/pages/cart.js
+++ js/pages/cart.js
@@ -1,62 +1,65 @@
 (() => {
-  // Stop script here if no product
-  if (!localStorage.getItem('shoppingCart')) return
+  const productsInShoppingCart = getProductInShoppingCart()
+  if (productsInShoppingCart === null) return
+  hydratePage(productsInShoppingCart)
+})()
 
-  // Remove no product text
+function getProductInShoppingCart() {
+  // Get product from local storage
+  return JSON.parse(localStorage.getItem('shoppingCart'))
+}
+
+function hydratePage(productsInShoppingCart) {
+  // Remove "no product" text
   document.getElementById('noProduct').style.display = 'none'
   
-  // Get product from local storage
-  const productsObject = JSON.parse(localStorage.getItem('shoppingCart'))
-
-  // Display all products
-  const productList = Object.values(productsObject)
+  // Loop over all products and displays them
+  const productList = Object.values(productsInShoppingCart)
   productList.forEach(product => {
     displayProduct(product)
   })
-})()
+}
 
 function displayProduct(product) {
-  // Get template
-  const templateElt = document.getElementById('product')
-
-  // Clone template
+  // Get & clone template
+  const templateElt = document.getElementById('productTemplate')
   const cloneElt = document.importNode(templateElt.content, true)
 
   // Hydrate template
-  const selectQuantityElt = cloneElt.getElementById('productQuantity')
-  // cloneElt.getElementById('productImage').src = product.imageUrl
   cloneElt.getElementById('productName').textContent = product.name
-  // cloneElt.getElementById('productDescription').textContent = product.description
-  selectQuantityElt.selectedIndex = product.quantity - 1
+  cloneElt.getElementById('productQuantity').selectedIndex = product.quantity - 1
 
-  // Add events on selected option change 
-  selectQuantityElt.onchange = (e) => {
+  // Add events
+  cloneElt.getElementById('productQuantity').onchange = (e) => {
     e.preventDefault()
-    Cart.updateProductQuantity(product._id, e.target.selectedIndex + 1)
+    Cart.updateProductQuantity(
+      product._id,
+      e.target.selectedIndex + 1
+    )
   }
 
   // Display template
   document.getElementById('productsList').appendChild(cloneElt)
 }
 
-function verifyForm() {
+// function verifyForm() {
 
-}
+// }
 
-function sendOrder() {
-  const orderJsonExample = {
-    "contact": {
-      "firstName": "Mathis",
-      "lastName": "Barré",
-      "address": "17 rue du 3 septembre 49320",
-      "city": "Gennes Val de Loire",
-      "email": "mathis.barre@live.fr"
-    },
-    "products": [
-      "5be9c8541c9d440000665243",
-      "5be9c8541c9d440000665243",
-      "5be9c8541c9d440000665243",
-      "5be9c8541c9d440000665243"
-    ]
-  }
-}
+// function sendOrder() {
+//   const orderJsonExample = {
+//     "contact": {
+//       "firstName": "Mathis",
+//       "lastName": "Barré",
+//       "address": "17 rue du 3 septembre 49320",
+//       "city": "Gennes Val de Loire",
+//       "email": "mathis.barre@live.fr"
+//     },
+//     "products": [
+//       "5be9c8541c9d440000665243",
+//       "5be9c8541c9d440000665243",
+//       "5be9c8541c9d440000665243",
+//       "5be9c8541c9d440000665243"
+//     ]
+//   }
+// }
