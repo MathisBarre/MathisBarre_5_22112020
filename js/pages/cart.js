918d89e38a04f876009ff1ffdb3bcfcff79928c1
Mathis Barré <mathis.barre@live.fr>
2021-02-12 12:12:50 +0100

    feat: start form validation

--- js/pages/cart.js
+++ js/pages/cart.js
@@ -1,4 +1,5 @@
 ;(() => {
+  console.log('Javascript is loaded')
   const productsInShoppingCart = getProductInShoppingCart()
   if (productsInShoppingCart === null) return
   hydratePage(productsInShoppingCart)
@@ -19,11 +20,7 @@ function hydratePage(productsInShoppingCart) {
     displayProduct(product)
   })
 
-  // Add page events listener
-  document.getElementById('confirmPurchase').onclick = (e) => {
-    e.preventDefault()
-    sendOrder()
-  }
+  addEventListeners()
 }
 
 function displayProduct(product) {
@@ -48,6 +45,55 @@ function displayProduct(product) {
 
   // Display template
   document.getElementById('productsList').appendChild(cloneElt)
+}
+
+function addEventListeners() {
+  // Purchase button
+  document.getElementById('confirmPurchase').onclick = (e) => {
+    e.preventDefault()
+    sendOrder()
+  }
+
+  // Form validation
+  const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  const zipcodeRegex = /[0-9]{5}(-[0-9]{4})?/
+
+  watchValidity(document.getElementById('firstname'), 'e.target.value.length > 1')
+  watchValidity(document.getElementById('lastname'), 'e.target.value.length > 1')
+  watchValidity(document.getElementById('email'), emailRegex + '.test(e.target.value)')
+  watchValidity(document.getElementById('adress'), 'e.target.value.length > 6')
+  watchValidity(document.getElementById('zipcode'), zipcodeRegex + '.test(e.target.value)')
+}
+
+function watchValidity(elt, condition) {
+  elt.oninput = (e) => {
+    if (eval(condition)) {
+      validInputElt(e.target)
+    } else {
+      neutralInputElt(e.target)
+    }
+  }
+
+  elt.onblur = (e) => {
+    if (!eval(condition)) {
+      invalidInputElt(e.target)
+    }
+  }
+}
+
+function validInputElt(elt) {
+  elt.style.border = 'solid 1px green'
+  elt.style.boxShadow = '#00800066 0px 0px 4px'
+}
+
+function invalidInputElt(elt) {
+  elt.style.border = 'solid 1px red'
+  elt.style.boxShadow = 'rgba(128, 0, 0, 0.4) 0px 0px 4px'
+}
+
+function neutralInputElt(elt) {
+  elt.style.border = ''
+  elt.style.boxShadow = ''
 }
 
 function sendOrder() {
